6fa188cd2633a9f01c40a48f309c596010d66f7b
afederi <alex404an@gmail.com>
2020-02-14 01:22:40 -0500

    Adding new file for february 14 2020

--- ProgrammingPractice/src/feb132020/HelloWorldFebruary.java
+++
@@ -1,8 +0,0 @@
-package feb132020;
-
-public class HelloWorldFebruary {
-	public static void main(String[]args){
-		System.out.println("Heppy Valentines Day!!!");
-	}
-
-}
---
+++ ProgrammingPractice/src/feb142020/kangarooProblem.java
@@ -0,0 +1,5 @@
+package feb142020;
+
+public class kangarooProblem {
+
+}
