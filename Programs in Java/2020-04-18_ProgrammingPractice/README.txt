ffe55d9e2944e93879ad4cb9e8e65fb5bcedcf74
afederi <alex404an@gmail.com>
2020-04-18 07:30:53 -0400

    More files to add

---
+++ Programs in Java/2020-04-18_ProgrammingPractice/README.txt
@@ -0,0 +1,1 @@
+This is a hello world test on repository set up
---
+++ tempDirectory/hw.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+  public static void main(String[]args){
+    System.out.println("THIS IS A HELLO WORLD Program"); // printing this output
+  }
+}
